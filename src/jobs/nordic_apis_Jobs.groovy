06f1d0200c0641473a7f44d408220ebcee11f8fa
Amit Patel <avpatel.257@gmail.com>
2021-01-24 12:30:44 -0500

    Delete nordic_apis_Jobs.groovy

--- src/jobs/nordic_apis_Jobs.groovy
+++
@@ -1,9 +0,0 @@
-import com.dslexample.builder.PipelineJobBuilder
-
-List developers = ['dev1@example.com', 'dev2@example.com']
-
-new PipelineJobBuilder()
-    .repoName("nordic-apis-2019")
-    .gitBranch("master")
-    .emails(developers)
-    .build(this)
